92f9df0ec3480ce4c7f47c128d89165dcc92764f
Rajesh-2025 <rajeshmadupathi1512@gmail.com>
2025-11-23 21:24:41 +0000

    Lab 4 - Rajesh

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
  * In a real-world application, you would want to use a cloud-based MongoDB service like MongoDB Atlas.
  */
 mongoose.connect(
-  process.env.MONGODB_URI || 'connectionstring for mongodb',
+  process.env.MONGODB_URI || 'mongodb+srv://rajeshm:Rajesh123@cluster0.b1ebqc2.mongodb.net/?appName=Cluster0',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
